0c1db549a94ffb1b3dcfacafe9b3ed2e41ffe5c8
Akenarin Komkoon <codingthailand@gmail.com>
2020-07-14 08:51:44 +0700

    day 1

---
+++ app/Http/Controllers/API/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index() {
+        return 'Date: ' . now() . config('app.name');
+    }
+
+    public function contact($email) {
+        return 'TOT Email: ' . $email;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+// http://localhost/tot_laravel7/public/api/
+Route::get('/', 'API\IndexController@index');
+Route::get('/contact/{email}', 'API\IndexController@contact');
+
+// http://localhost/tot_laravel7/public/api/product/3/comment/34
+Route::get('/product/{id}/comment/{comment_id}', function($id, $comment_id) {
+    return 'product id '.$id. ' comment '.$comment_id; //3 34
+});
+
+//optional parameters ?
+Route::get('/staff/{name?}', function($name = 'Bob') {
+    return 'staff ' . $name;
+});
+
+// api/admin
+Route::prefix('admin')->group(function() {
+    Route::get('/', function() {
+        return 'admin index';
+    });
+    Route::get('/profile', function() {
+        return 'admin profile';
+    });
+    Route::get('/book', function() {
+        return 'admin book';
+    });
+});
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
